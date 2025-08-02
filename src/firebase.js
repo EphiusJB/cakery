5a318336794e1c17a913e91bab3255fa8b12dd47
Ephius Mutambo <114339494+EphiusJB@users.noreply.github.com>
2025-08-02 13:18:31 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -17,13 +17,6 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAYuQ2s06tyxecTCpG_sDglm3eDPsYl92I",
-  authDomain: "mikas-77fe7.firebaseapp.com",
-  projectId: "mikas-77fe7",
-  storageBucket: "mikas-77fe7.firebasestorage.app",
-  messagingSenderId: "155048517528",
-  appId: "1:155048517528:web:4003b2b18a0c4d316455ed",
-  measurementId: "G-VLWL63M7HH"
 };
 
 // Initialize Firebase
